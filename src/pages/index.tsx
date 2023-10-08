176c0a6f910b62e681172de6b6c4c99851eaee26
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2023-10-08 23:04:55 +0800

    Posting loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,11 @@ import Image  from 'next/image'
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { LoadingPage} from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
+
+
 
 dayjs.extend(relativeTime);
 
@@ -23,6 +26,16 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]){
+        toast.error(errorMessage[0]);
+      }
+      else {
+        toast.error("Failed to post! Please try again later.")
+      }
+      
     }
   });
 
@@ -38,9 +51,24 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={() => mutate({ content: input })}> Post </button>
 
+        onKeyDown={(e) => {
+          if (e.key == "Enter"){
+            e.preventDefault();
+            if (input !== ""){
+              mutate({ content: input });
+            }
+          }
+        }}
+      />
+      {input !== "" && !isPosting && (<button onClick={() => mutate({ content: input })} disabled={isPosting}> Post </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   )
 }
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -47,7 +47,7 @@ export const postsRouter = createTRPCRouter({
 
   create: privateProcedure
   .input(z.object({
-    content: z.string().emoji().min(1).max(200),
+    content: z.string().emoji("Only emojis are allowed").min(1).max(200),
   })
   ).mutation( async ({ ctx, input }) => {
     const authorId = ctx.userId;
@@ -55,7 +55,7 @@ export const postsRouter = createTRPCRouter({
     const { success } = await ratelimit.limit(authorId);
 
     if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-    
+
     const post = await ctx.prisma.post.create({
       data: {
         authorId, 
