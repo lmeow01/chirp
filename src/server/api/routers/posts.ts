ddea8a16452afbf9acbbeb7f486de85832addf8b
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2023-10-08 22:49:37 +0800

    Ratelimit the endpoint

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,6 +3,8 @@ import type { User } from "@clerk/nextjs/server"
 import { z } from "zod";
 import { TRPCError } from '@trpc/server';
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis/nodejs";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -11,6 +13,12 @@ const filterUserForClient = (user: User) => {
     imageUrl: user.imageUrl
   }
 }
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true
+})
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
@@ -18,7 +26,6 @@ export const postsRouter = createTRPCRouter({
       orderBy: [{ createdAt: "desc" }]
     });
 
-  
     const users = (
       await clerkClient.users.getUserList({
       userId: posts.map((post) => post.authorId),
@@ -45,6 +52,10 @@ export const postsRouter = createTRPCRouter({
   ).mutation( async ({ ctx, input }) => {
     const authorId = ctx.userId;
 
+    const { success } = await ratelimit.limit(authorId);
+
+    if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+    
     const post = await ctx.prisma.post.create({
       data: {
         authorId, 
